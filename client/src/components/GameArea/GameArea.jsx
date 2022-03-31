0d4a26b9ce8837b127fd7170c1ab88f0498bfcf6
Nikita <mnextn@mail.ru>
2022-03-31 21:48:44 +0300

    day1

--- client/src/components/GameArea/GameArea.jsx
+++ client/src/components/GameArea/GameArea.jsx
@@ -10,57 +10,58 @@ function GameArea(props) {
 
     useEffect(()=>{
         async function getGame(){
-            const res = await logoutAxios
-            setQuestion(res)
+            const res = await logoutAxios()
+            setQuestion(res.data)
+            console.log(question);
         }
         getGame()
     },[])
 
-    console.log(question);
+
 
 
     return (
         <div className="container col-6">
-            <h1 className="text-center mb-5">СВОЯ ИГРА</h1>
-            <table className={`table ${style.tablemain}`}>
-                <tbody>
-                    {topics.length ? (
-                        topics.map((topic) => {
-                            return (
-                                <tr
-                                    className={`${style.trmain} ${style.thborder} `}
-                                    key={topic.id}
-                                >
-                                    <th scope="row" className="">
-                                        <h2>{topic.topic_name}</h2>
-                                    </th>
-                                    {questions.filter(
-                                        (question) =>
-                                            question.id_topic === topic.id
-                                    ).length ? (
-                                        questions
-                                            .filter(
-                                                (question) =>
-                                                    question.id_topic ===
-                                                    topic.id
-                                            )
-                                            .map((question) => (
-                                                <GameQuestion
-                                                    key={question.id}
-                                                    question={question}
-                                                />
-                                            ))
-                                    ) : (
-                                        <p>NO TOPIC DATA IS LOADED</p>
-                                    )}
-                                </tr>
-                            );
-                        })
-                    ) : (
-                        <p>NO GAME DATA IS LOADED</p>
-                    )}
-                </tbody>
-            </table>
+            {/*<h1 className="text-center mb-5">СВОЯ ИГРА</h1>*/}
+            {/*<table className={`table ${style.tablemain}`}>*/}
+            {/*    <tbody>*/}
+            {/*        {topics.length ? (*/}
+            {/*            topics.map((topic) => {*/}
+            {/*                return (*/}
+            {/*                    <tr*/}
+            {/*                        className={`${style.trmain} ${style.thborder} `}*/}
+            {/*                        key={topic.id}*/}
+            {/*                    >*/}
+            {/*                        <th scope="row" className="">*/}
+            {/*                            <h2>{topic.topic_name}</h2>*/}
+            {/*                        </th>*/}
+            {/*                        {questions.filter(*/}
+            {/*                            (question) =>*/}
+            {/*                                question.id_topic === topic.id*/}
+            {/*                        ).length ? (*/}
+            {/*                            questions*/}
+            {/*                                .filter(*/}
+            {/*                                    (question) =>*/}
+            {/*                                        question.id_topic ===*/}
+            {/*                                        topic.id*/}
+            {/*                                )*/}
+            {/*                                .map((question) => (*/}
+            {/*                                    <GameQuestion*/}
+            {/*                                        key={question.id}*/}
+            {/*                                        question={question}*/}
+            {/*                                    />*/}
+            {/*                                ))*/}
+            {/*                        ) : (*/}
+            {/*                            <p>NO TOPIC DATA IS LOADED</p>*/}
+            {/*                        )}*/}
+            {/*                    </tr>*/}
+            {/*                );*/}
+            {/*            })*/}
+            {/*        ) : (*/}
+            {/*            <p>NO GAME DATA IS LOADED</p>*/}
+            {/*        )}*/}
+            {/*    </tbody>*/}
+            {/*</table>*/}
         </div>
     );
 }
