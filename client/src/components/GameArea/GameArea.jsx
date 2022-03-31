92c2a2a80076ec6cbf2c0cda82a84c13e96a7e81
kolombo306 <gruzintsev@ro.ru>
2022-03-31 21:29:33 +0300

    gamearea v2

--- client/src/components/GameArea/GameArea.jsx
+++ client/src/components/GameArea/GameArea.jsx
@@ -3,77 +3,77 @@ import GameQuestion from '../GameQuestion/GameQuestion';
 import style from './GameArea.module.css';
 
 function GameArea(props) {
-    const topics = [
-        { id: 1, topic_name: 'Про Эльбрус' },
-        { id: 2, topic_name: 'Животные' },
-        { id: 3, topic_name: 'Знаменитости' },
-    ];
+    // const topics = [
+    //     { id: 1, topic_name: 'Про Эльбрус' },
+    //     { id: 2, topic_name: 'Животные' },
+    //     { id: 3, topic_name: 'Знаменитости' },
+    // ];
 
-    const questions = [
-        {
-            id: 1,
-            question_text: 'Вопрос Эльбрус 1',
-            question_price: 100,
-            question_answer: 'answer',
-            id_topic: 1,
-        },
-        {
-            id: 2,
-            question_text: 'Вопрос Эльбрус 2',
-            question_price: 300,
-            question_answer: 'answer',
-            id_topic: 1,
-        },
-        {
-            id: 3,
-            question_text: 'Вопрос Эльбрус 3',
-            question_price: 500,
-            question_answer: 'answer',
-            id_topic: 1,
-        },
-        {
-            id: 4,
-            question_text: 'Вопрос Животные 1',
-            question_price: 100,
-            question_answer: 'answer',
-            id_topic: 2,
-        },
-        {
-            id: 5,
-            question_text: 'Вопрос Животные 2',
-            question_price: 300,
-            question_answer: 'answer',
-            id_topic: 2,
-        },
-        {
-            id: 6,
-            question_text: 'Вопрос Животные 3',
-            question_price: 500,
-            question_answer: 'answer',
-            id_topic: 2,
-        },
-        {
-            id: 7,
-            question_text: 'Вопрос Знаменитости 1',
-            question_price: 100,
-            question_answer: 'answer',
-            id_topic: 3,
-        },
-        {
-            id: 8,
-            question_text: 'Вопрос Знаменитости 2',
-            question_price: 300,
-            question_answer: 'answer',
-            id_topic: 3,
-        },
-        {
-            id: 9,
-            question_text: 'Вопрос Знаменитости 3',
-            question_price: 500,
-            question_answer: 'answer',
-            id_topic: 3,
-        },
-    ];
+    // const questions = [
+    //     {
+    //         id: 1,
+    //         question_text: 'Вопрос Эльбрус 1',
+    //         question_price: 100,
+    //         question_answer: 'answer',
+    //         id_topic: 1,
+    //     },
+    //     {
+    //         id: 2,
+    //         question_text: 'Вопрос Эльбрус 2',
+    //         question_price: 300,
+    //         question_answer: 'answer',
+    //         id_topic: 1,
+    //     },
+    //     {
+    //         id: 3,
+    //         question_text: 'Вопрос Эльбрус 3',
+    //         question_price: 500,
+    //         question_answer: 'answer',
+    //         id_topic: 1,
+    //     },
+    //     {
+    //         id: 4,
+    //         question_text: 'Вопрос Животные 1',
+    //         question_price: 100,
+    //         question_answer: 'answer',
+    //         id_topic: 2,
+    //     },
+    //     {
+    //         id: 5,
+    //         question_text: 'Вопрос Животные 2',
+    //         question_price: 300,
+    //         question_answer: 'answer',
+    //         id_topic: 2,
+    //     },
+    //     {
+    //         id: 6,
+    //         question_text: 'Вопрос Животные 3',
+    //         question_price: 500,
+    //         question_answer: 'answer',
+    //         id_topic: 2,
+    //     },
+    //     {
+    //         id: 7,
+    //         question_text: 'Вопрос Знаменитости 1',
+    //         question_price: 100,
+    //         question_answer: 'answer',
+    //         id_topic: 3,
+    //     },
+    //     {
+    //         id: 8,
+    //         question_text: 'Вопрос Знаменитости 2',
+    //         question_price: 300,
+    //         question_answer: 'answer',
+    //         id_topic: 3,
+    //     },
+    //     {
+    //         id: 9,
+    //         question_text: 'Вопрос Знаменитости 3',
+    //         question_price: 500,
+    //         question_answer: 'answer',
+    //         id_topic: 3,
+    //     },
+    // ];
 
     return (
         <div className="container col-6">
